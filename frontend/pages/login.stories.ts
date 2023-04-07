0485c9a20074634d3c86a6105d35ddae5aa8d30c
Julian <66846059+Julian702@users.noreply.github.com>
2023-04-07 12:18:30 +0200

    added stories for the login page

---
+++ frontend/pages/login.stories.ts
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { within, userEvent } from '@storybook/testing-library';
+
+import Page from './login';
+
+const meta: Meta<typeof Page> = {
+  title: 'Pages/Access',
+  component: Page,
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Page>;
+
+export const Login: Story = {};
+
+export const Register: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const registerButton = await canvas.getByText('or register');
+    await userEvent.click(registerButton);
+  },
+};

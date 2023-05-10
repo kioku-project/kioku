4fe33dcb7bdd0b6c52ca9965401fc70c186d193c
Julian <66846059+Julian702@users.noreply.github.com>
2023-05-10 09:42:28 +0200

    updated storybook and added story sorting

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -1,26 +1,24 @@
-import type { StorybookConfig } from '@storybook/nextjs';
+import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-    "../components/**/*.stories.@(js|jsx|ts|tsx)",
-    "../pages/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": {
-    "name": "@storybook/nextjs",
-    "options": {}
-  },
-  "docs": {
-    "autodocs": "tag"
-  }, 
-  "staticDirs": [
-    '../public'
-  ],
+	stories: [
+		"../stories/**/*.mdx",
+		"../stories/**/*.stories.@(js|jsx|ts|tsx)",
+		"../components/**/*.stories.@(js|jsx|ts|tsx)",
+		"../pages/**/*.stories.@(js|jsx|ts|tsx)",
+	],
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/addon-interactions",
+	],
+	framework: {
+		name: "@storybook/nextjs",
+		options: {},
+	},
+	docs: {
+		autodocs: "tag",
+	},
+	staticDirs: ["../public"],
 };
 export default config;
--- frontend/.storybook/preview.ts
+++ frontend/.storybook/preview.ts
@@ -1,11 +1,25 @@
-import "../styles/globals.css"
+import "../styles/globals.css";
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+	actions: { argTypesRegex: "^on[A-Z].*" },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+	options: {
+		storySort: {
+			method: "alphabetical",
+			order: [
+				"Example",
+				"Pages",
+				"Components",
+				"Navigation",
+				"Input",
+				"Form",
+				"Graphics",
+			],
+		},
+	},
+};

a59525b8380be12d441fec39489f1ed5e3fee080
memeToasty <memeToasty@gmail.com>
2023-06-12 18:43:56 +0200

    added tests

--- backend/go.mod
+++ backend/go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.3.1 // indirect
@@ -30,6 +31,7 @@ require (
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/philhofer/fwd v1.1.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/rogpeppe/go-internal v1.10.0 // indirect
 	github.com/savsgio/dictpool v0.0.0-20221023140959-7bf2e61cea94 // indirect
@@ -40,6 +42,7 @@ require (
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20230525234020-1aefcd67740a // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230530153820-e85fd2cbaebc // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
@@ -74,8 +77,9 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/sergi/go-diff v1.3.1 // indirect
-	github.com/skeema/knownhosts v1.1.1 // indirect
-	github.com/urfave/cli/v2 v2.25.5 // indirect
+	github.com/skeema/knownhosts v1.1.0 // indirect
+	github.com/stretchr/testify v1.8.4
+	github.com/urfave/cli/v2 v2.25.3 // indirect
 	github.com/xanzy/ssh-agent v0.3.3 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/crypto v0.9.0
---
+++ backend/pkg/helper/errors_test.go
@@ -0,0 +1,46 @@
+package helper_test
+
+import (
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/kioku-project/kioku/pkg/helper"
+	"github.com/stretchr/testify/assert"
+	microErrors "go-micro.dev/v4/errors"
+)
+
+const (
+	id = "id"
+)
+
+func TestErrors(t *testing.T) {
+	microNotAuthorizedErr := helper.NewMicroNotAuthorizedErr(id)
+	microNoEntryWithIDErr := helper.NewMicroNoEntryWithIDErr(id)
+	microNoExistingUserWithEmailErr := helper.NewMicroNoExistingUserWithEmailErr(id)
+	microUserAlreadyExistsErr := helper.NewMicroUserAlreadyExistsErr(id)
+	microInvalidUserNameFormatErr := helper.NewMicroInvalidUserNameFormatErr(id)
+	microInvalidParameterDataErr := helper.NewMicroInvalidParameterDataErr(id)
+	microUserAlreadyInGroupErr := helper.NewMicroUserAlreadyInGroupErr(id)
+	microUserAdmissionInProgressErr := helper.NewMicroUserAdmissionInProgressErr(id)
+	microCardSideNotInGivenCardErr := helper.NewMicroCardSideNotInGivenCardErr(id)
+	microHashingFailedErr := helper.NewMicroHashingFailedErr(id)
+	microInvalidEmailOrPasswordErr := helper.NewMicroInvalidEmailOrPasswordErr(id)
+	microNotSuccessfulResponseErr := helper.NewMicroNotSuccessfulResponseErr(id)
+	fiberBadRequestErr := helper.NewFiberBadRequestErr(id)
+	fiberUnauthorizedErr := helper.NewFiberUnauthorizedErr(id)
+	
+	assert.IsType(t, microErrors.Unauthorized(string(id), "user not authorized"), microNotAuthorizedErr)
+	assert.IsType(t, microErrors.BadRequest(string(id), "no entry with id"), microNoEntryWithIDErr)
+	assert.IsType(t, microErrors.BadRequest(string(id), "no existing user with email"), microNoExistingUserWithEmailErr)
+	assert.IsType(t, microErrors.BadRequest(string(id), "this user already exists"), microUserAlreadyExistsErr)
+	assert.IsType(t, microErrors.BadRequest(string(id), "invalid user name format"), microInvalidUserNameFormatErr)
+	assert.IsType(t, microErrors.BadRequest(string(id), "invalid parameter data"), microInvalidParameterDataErr)
+	assert.IsType(t, microErrors.BadRequest(string(id), "user already in group"), microUserAlreadyInGroupErr)
+	assert.IsType(t, microErrors.BadRequest(string(id), "user already invited"), microUserAdmissionInProgressErr)
+	assert.IsType(t, microErrors.BadRequest(string(id), "card side not in given card"), microCardSideNotInGivenCardErr)
+	assert.IsType(t, microErrors.InternalServerError(string(id), "error while hashing password"), microHashingFailedErr)
+	assert.IsType(t, microErrors.BadRequest(string(id), "invalid email or password"), microInvalidEmailOrPasswordErr)
+	assert.IsType(t, microErrors.BadRequest(string(id), "operation not successful"), microNotSuccessfulResponseErr)
+	assert.IsType(t, fiber.NewError(fiber.StatusBadRequest, id), fiberBadRequestErr)
+	assert.IsType(t, fiber.NewError(fiber.StatusUnauthorized, id), fiberUnauthorizedErr)
+}

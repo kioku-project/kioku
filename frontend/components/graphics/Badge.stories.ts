61e4af7d33d859e72db1bd4f2c1eb6b42007a572
Julian <66846059+Julian702@users.noreply.github.com>
2023-06-12 12:45:37 +0200

    added Badge component

---
+++ frontend/components/graphics/Badge.stories.ts
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Badge } from "./Badge";
+
+const meta: Meta<typeof Badge> = {
+	title: "Components/Badge",
+	component: Badge,
+	tags: ["autodocs"],
+	args: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof Badge>;
+
+export const Primary: Story = {
+	args: {
+		label: "Primary",
+		style: "primary",
+	},
+};
+
+export const Secondary: Story = {
+	args: {
+		label: "Secondary",
+		style: "secondary",
+	},
+};
+
+export const Tertiary: Story = {
+	args: {
+		label: "Tertiary",
+		style: "tertiary",
+	},
+};

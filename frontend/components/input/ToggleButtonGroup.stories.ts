396fc149016cd15b35f86c958a022dc1bd588188
Julian <66846059+Julian702@users.noreply.github.com>
2023-11-07 14:56:30 +0100

    Add ToggleButtonGroup component

---
+++ frontend/components/input/ToggleButtonGroup.stories.ts
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { ToggleButtonGroup } from "./ToggleButtonGroup";
+
+const meta: Meta<typeof ToggleButtonGroup> = {
+	title: "Input/ToggleButtonGroup",
+	component: ToggleButtonGroup,
+	tags: ["autodocs"],
+	args: {
+		id: "ButtonId",
+		choices: ["ONE", "TWO", "THREE"],
+	},
+};
+
+export default meta;
+type Story = StoryObj<typeof ToggleButtonGroup>;
+
+export const Primary: Story = {
+	args: {
+		activeButton: "ONE",
+		activeButtonStyle: "primary",
+	},
+};
+
+export const Warning: Story = {
+	args: {
+		activeButton: "TWO",
+		activeButtonStyle: "warning"
+	},
+};
+
+export const Error: Story = {
+	args: {
+		activeButton: "THREE",
+		activeButtonStyle: "error"
+	},
+};
+
+export const Disabled: Story = {
+	args: {
+		activeButton: "ONE",
+		disabled: true
+	},
+};

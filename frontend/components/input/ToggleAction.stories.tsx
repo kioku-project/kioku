ed48919f2c5a83cb6cdf3b46e8e5805d003ec23d
Julian <66846059+Julian702@users.noreply.github.com>
2023-11-07 15:19:19 +0100

    Add ToggleAction component

---
+++ frontend/components/input/ToggleAction.stories.tsx
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { ToggleAction } from "./ToggleAction";
+
+const meta: Meta<typeof ToggleAction> = {
+	title: "Input/ToggleAction",
+	component: ToggleAction,
+	tags: ["autodocs"],
+	args: {
+		id: "toggleActionId",
+		choices: ["ONE", "TWO", "THREE"],
+	},
+};
+
+export default meta;
+type Story = StoryObj<typeof ToggleAction>;
+
+export const Default: Story = {
+	args: {
+		header: "ToggleAction",
+		description: "Choose one option",
+		activeButton: "ONE",
+	},
+};
+
+export const Disabled: Story = {
+	args: {
+		header: "ToggleAction",
+		description: "This one is disabled",
+		disabled: true,
+		activeButton: "TWO",
+	},
+};
+
+export const Header: Story = {
+	args: {
+		header: "ToggleAction",
+		activeButton: "THREE",
+	},
+};
+
+export const Description: Story = {
+	args: {
+		description: "Choose one option",
+	},
+};

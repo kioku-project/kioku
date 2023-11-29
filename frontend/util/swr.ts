9d418785fd8b3c6181cfe1ea1047191e363f2e75
Julian Herzog <66846059+Julian702@users.noreply.github.com>
2023-11-29 08:49:36 +0100

    Add dashboard tab (#202)
    * Adjust deck dueCards endpoint to backend changes
    
    * Add dashboard tab
    
    * Add translations
    
    * Rename active and favorite to isActive and isFavorite
    
    * Fix code smells
    
    * Apply pr feedback

--- frontend/util/swr.ts
+++ frontend/util/swr.ts
@@ -112,6 +112,28 @@ export function useDecks(groupID?: string) {
 	};
 }
 
+export function useFavoriteDecks() {
+	const { data, error, isLoading } = useSWR<{
+		decks: Deck[];
+	}>(`/api/decks/favorites`, fetcher);
+	return {
+		decks: data?.decks,
+		error,
+		isLoading,
+	};
+}
+
+export function useActiveDecks() {
+	const { data, error, isLoading } = useSWR<{
+		decks: Deck[];
+	}>(`/api/decks/active`, fetcher);
+	return {
+		decks: data?.decks,
+		error,
+		isLoading,
+	};
+}
+
 export function useDeck(deckID?: string) {
 	const { data, error, isLoading } = useSWR<Deck>(
 		deckID ? `/api/decks/${deckID}` : null,
@@ -125,12 +147,12 @@ export function useDeck(deckID?: string) {
 }
 
 export function useDueCards(deckID?: string) {
-	const { data, error, isLoading } = useSWR<number>(
+	const { data, error, isLoading } = useSWR<{ dueCards: number }>(
 		deckID ? `/api/decks/${deckID}/dueCards` : null,
 		fetcher
 	);
 	return {
-		dueCards: data,
+		dueCards: data?.dueCards,
 		error,
 		isLoading,
 	};

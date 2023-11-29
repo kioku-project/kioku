8f48a54066592acce5807f8781f151f42f7a07fc
Julian <66846059+Julian702@users.noreply.github.com>
2023-11-29 20:13:48 +0100

    Fix login issue

--- frontend/util/reauth.ts
+++ frontend/util/reauth.ts
@@ -1,42 +1,29 @@
-import { getCookie, hasCookie } from "cookies-next";
-import jwtDecode, { JwtPayload } from "jwt-decode";
+import { getCookie } from "cookies-next";
 
 // Returns true if reauth successful, false if relocation in progress
 export async function reauth(): Promise<boolean> {
-	const answ = await fetch("/api/reauth", {
+	const response = await fetch("/api/reauth", {
 		credentials: "include",
 	});
-	if (answ.status !== 200) {
+	if (response.status !== 200) {
 		window.location.replace("/login");
 	}
-	return answ.status === 200;
-}
-
-export function checkAccessTokenValid() {
-	if (!hasCookie("access_token")) {
-		return false;
-	}
-	const accessToken = getCookie("access_token");
-	const decoded = jwtDecode<JwtPayload>(accessToken!.toString());
-	if (!decoded.exp || decoded.exp < Math.floor(Date.now() / 1000)) {
-		return false;
-	}
-	return true;
+	return response.status === 200;
 }
 
 export async function authedFetch(
 	input: RequestInfo | URL,
 	init?: RequestInit | undefined
 ) {
-	if (!checkAccessTokenValid()) {
-		await reauth();
-	}
-	const answ = await fetch(input, {
+	const response = await fetch(input, {
 		...init,
 		headers: {
 			...init?.headers,
 			Authorization: "Bearer " + getCookie("access_token"),
 		},
 	});
-	return answ;
+	if (response.status === 401) {
+		await reauth();
+	}
+	return response;
 }

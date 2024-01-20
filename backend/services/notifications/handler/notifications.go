2955433925f6e45faa04a514b87a3cc8cf9e432e
TomRomeo <tom.romeo.doil@gmail.com>
2024-01-20 20:47:12 +0100

    Add initial welcome notification

--- backend/services/notifications/handler/notifications.go
+++ backend/services/notifications/handler/notifications.go
@@ -2,7 +2,10 @@ package handler
 
 import (
 	"context"
+	"encoding/json"
+	"os"
 
+	"github.com/SherClockHolmes/webpush-go"
 	"github.com/kioku-project/kioku/pkg/model"
 	pbCommon "github.com/kioku-project/kioku/pkg/proto"
 	pbSrs "github.com/kioku-project/kioku/services/srs/proto"
@@ -32,6 +35,44 @@ func (e *Notifications) Enroll(ctx context.Context, req *pb.PushSubscriptionRequ
 	if err := e.store.CreatePushSubscription(ctx, subscription); err != nil {
 		return err
 	}
+	privateKey, success := os.LookupEnv("VAPID_PRIVATE_KEY")
+	if !success {
+		logger.Fatal("VAPID_PRIVATE_KEY environment variable not set")
+	}
+	publicKey, success := os.LookupEnv("VAPID_PUBLIC_KEY")
+	if !success {
+		logger.Fatal("VAPID_PUBLIC_KEY not set")
+	}
+	s := &webpush.Subscription{
+		Endpoint: subscription.Endpoint,
+		Keys: webpush.Keys{
+			P256dh: subscription.P256DH,
+			Auth:   subscription.Auth,
+		},
+	}
+	notification := &model.PushNotification{
+		Title: "Welcome to Kioku!",
+		Options: model.PushNotificationOptions{
+			Body:    "You will now receive reminders so you don't forget your cards'!",
+			Actions: []map[string]string{},
+			Vibrate: []int{200, 100, 200},
+			Tag:     "Kioku",
+		},
+	}
+	jsonNotification, err := json.Marshal(notification)
+	if err != nil {
+		return err
+	}
+	resp, err := webpush.SendNotification(jsonNotification, s, &webpush.Options{
+		Subscriber:      "web-push@kioku.dev",
+		VAPIDPublicKey:  publicKey,
+		VAPIDPrivateKey: privateKey,
+		TTL:             30,
+	})
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
 	rsp.Success = true
 	return nil
 }
